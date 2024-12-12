0b84eb9344f1f3aa1f4391b19d388048c93f4bdf
kehinde-odunuga <omobolaji92@gmail.com>
2024-12-12 22:44:15 +0100

    database setup

--- buzzify/settings.py
+++ buzzify/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'buzzify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'buzzify',
+        'USER': 'odunuga',
+        'PASSWORD': 'odunuga', 
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
